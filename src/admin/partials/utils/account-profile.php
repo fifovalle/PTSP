5094896d8aa9edfd1aeaf2cd4de6018d766e387e
fifovalle <fifanaufal10@gmail.com>
2024-02-26 09:43:10 +0700

    Memodifikasi file profile

--- src/admin/partials/utils/account-profile.php
+++ src/admin/partials/utils/account-profile.php
@@ -1,7 +1,120 @@
 <div class="dataAccount">
-    <div class="card">
-        <div class="card-body">
-
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div>
+                            <h5 class="card-title fw-bold">Informasi Anda</h5>
+                        </div>
+                        <hr>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Nama Depan</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            Naufal
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Nama Belakang</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            FIFA
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Nama Pengguna</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            zonaDeveloper
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Email</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            fifanaufal10@gmail.com
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">No Telpon</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            (+62) 812-1234-5678
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Alamat</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            Bandung
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div>
+                            <h5 class="card-title fw-bold">Informasi Status</h5>
+                        </div>
+                        <hr>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Peran Anda</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <h6 class="mb-0">Super Admin</h6>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Status Verifikasi</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <span class="badge text-bg-success">Terverifikasi</span>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Jenis Kelamin</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <h6 class="mb-0">Pria</h6>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 class="mb-0">Kata Sandi</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <h6 class="mb-0">NaufalGanteng123.</h6>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- src/admin/partials/utils/edit-account-profile.php
+++ src/admin/partials/utils/edit-account-profile.php
@@ -1,33 +1,67 @@
 <div class="dataEditAccount">
     <div class="card">
         <div class="card-body">
-            <form action="#" method="get">
-                <div class="form-group">
-                    <label for="name" class="form-label">Name</label>
-                    <input type="text" name="name" id="name" class="form-control" placeholder="Your Name" value="John Doe">
+            <form method="POST" action="<?php echo $akarUrl; ?>src/admin/config/add-admin.php" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label for="fotoAdmin" class="form-label">Foto</label>
+                    <input type="file" class="form-control" id="fotoAdmin" name="Foto_Admin" autocomplete="off">
                 </div>
-                <div class="form-group">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="text" name="email" id="email" class="form-control" placeholder="Your Email" value="john.doe@example.net">
+                <div class="mb-3">
+                    <label for="frontNameAdmin" class="form-label">Nama Depan</label>
+                    <input type="text" class="form-control inputData" placeholder="Masukan Nama Depan Admin" id="frontNameAdmin" name="Nama_Depan_Admin" autocomplete="off">
                 </div>
-                <div class="form-group">
-                    <label for="phone" class="form-label">Phone</label>
-                    <input type="text" name="phone" id="phone" class="form-control" placeholder="Your Phone" value="083xxxxxxxxx">
+                <div class="mb-3">
+                    <label for="lastNameAdmin" class="form-label">Nama Belakang</label>
+                    <input type="text" class="form-control inputData" placeholder="Masukan Nama Belakang Admin" id="lastNameAdmin" name="Nama_Belakang_Admin" autocomplete="off">
                 </div>
-                <div class="form-group">
-                    <label for="birthday" class="form-label">Birthday</label>
-                    <input type="date" name="birthday" id="birthday" class="form-control" placeholder="Your Birthday">
+                <div class="mb-3">
+                    <label for="userNameAdmin" class="form-label">Nama Pengguna</label>
+                    <input type="text" class="form-control inputData" placeholder="Masukan Nama Pengguna Admin" id="userNameAdmin" name="Nama_Pengguna_Admin" autocomplete="off">
                 </div>
-                <div class="form-group">
-                    <label for="gender" class="form-label">Gender</label>
-                    <select name="gender" id="gender" class="form-control">
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
+                <div class="mb-3">
+                    <label for="emailAdmin" class="form-label">Email</label>
+                    <input type="email" class="form-control inputData" placeholder="Masukan Email Admin" id="emailAdmin" name="Email_Admin" autocomplete="off">
+                </div>
+                <div class="mb-3 parentPassword">
+                    <label for="passwordAdmin" class="form-label">Kata Sandi</label>
+                    <input type="password" class="form-control inputData" placeholder="&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;" id="passwordAdmin" name="Kata_Sandi">
+                    <i class="fas fa-eye iconInputContainer" id="togglePasswordAdmin"></i>
+                </div>
+                <div class="mb-3 parentConfirmPassword">
+                    <label for="confirmPasswordAdmin" class="form-label">Konfirmasi Kata Sandi</label>
+                    <input type="password" class="form-control inputData" placeholder="&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;&#x002A;" id="confirmPasswordAdmin" name="Konfirmasi_Kata_Sandi">
+                    <i class="fas fa-eye iconInputContainer" id="toggleConfirmPasswordAdmin"></i>
+                </div>
+                <div class="mb-3">
+                    <label for="numberAdmin" class="form-label">Nomor Telepon</label>
+                    <div class="input-group">
+                        <span class="input-group-text spanNumberData">+62</span>
+                        <input type="number" placeholder="Masukan Nomor Telepon Admin" class="form-control inputData" id="numberAdmin" name="No_Telepon_Admin" autocomplete="off">
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="genderAdmin" class="form-label">Jenis Kelamin</label>
+                    <select name="Jenis_Kelamin_Admin" id="genderAdmin" class="form-control inputData">
+                        <option value="" selected>Pilih Jenis Kelamin Admin</option>
+                        <option value="Pria">Pria</option>
+                        <option value="Wanita">Wanita</option>
                     </select>
                 </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                <div class="mb-3">
+                    <label for="roleAdmin" class="form-label">Peran Admin</label>
+                    <select name="Peran_Admin" id="roleAdmin" class="form-control inputData">
+                        <option value="" selected>Pilih Peran Admin</option>
+                        <option value="1">Super Admin</option>
+                        <option value="2">Instansi A</option>
+                        <option value="3">Instansi B</option>
+                        <option value="4">Instansi C</option>
+                    </select>
                 </div>
+                <div class="mb-3">
+                    <label for="addressAdmin" class="form-label">Alamat Admin</label>
+                    <textarea name="Alamat_Admin" placeholder="Masukan Alamat Admin" class="form-control inputData addressAddAdmin" id="addressAdmin" autocomplete="off"></textarea>
+                </div>
+                <button type="submit" class="btn btnUpload" name="Simpan">Kirim</button>
             </form>
         </div>
     </div>
