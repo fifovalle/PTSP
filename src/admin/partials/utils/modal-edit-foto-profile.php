a78cd46ffdcb6c665b502afc4f235a4723bfb466
fifovalle <fifanaufal10@gmail.com>
2024-02-26 14:46:11 +0700

    Memodifikasi file file

---
+++ src/admin/partials/utils/modal-edit-foto-profile.php
@@ -0,0 +1,30 @@
+<?php
+// AKAR TAUTAN
+$akarUrl = "http://localhost/PTSP/";
+// HALAMAN SAAT DIBUKA
+$halamanSaatIni = basename($_SERVER['PHP_SELF']);
+?>
+<div class="modal fade" id="modalSuntingFoto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Sunting Foto Profil</h1>
+                <div class="modalClose" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="fas fa-times"></i>
+                </div>
+            </div>
+            <div class="modal-body text-center">
+                <form>
+                    <div class="form-group">
+                        <label class="fw-bold unggahFoto" for="unggahFoto">Unggah Foto</label>
+                        <label for="unggahFoto" class="upload-icon">
+                            <i class="bx bx-plus"></i>
+                        </label>
+                        <input type="file" class="form-control-file visually-hidden" id="unggahFoto">
+                    </div>
+                    <button type="submit" class="btn btnUpload" name="Simpan">Simpan</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
