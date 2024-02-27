757deb28e7d9ab6fcb787fa691b17c7a96dc75eb
fifovalle <fifanaufal10@gmail.com>
2024-02-27 20:39:52 +0700

    Memodifikasi file logout

--- src/admin/config/logout.php
+++ src/admin/config/logout.php
@@ -4,5 +4,5 @@ $halamanSaatIni = basename($_SERVER['PHP_SELF']);
 session_start();
 session_unset();
 session_destroy();
-header("Location: $akarUrl/src/admin/pages/login.php");
+header("Location: $akarUrl" . "src/admin/pages/login.php");
 exit;
