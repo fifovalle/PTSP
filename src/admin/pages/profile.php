539a286fb3bd617452a757242bca8e41113f7a87
fifovalle <fifanaufal10@gmail.com>
2024-02-29 09:42:23 +0700

    Memodifikasi file profile

--- src/admin/pages/profile.php
+++ src/admin/pages/profile.php
@@ -42,12 +42,12 @@ include '../config/databases.php';
                     <div class="d-flex justify-content-between align-items-center text-center">
                         <h1 class="breadcrumb">Profil Saya</h1>
                     </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-lg-12 mb-2">
                             <img src="../assets/image/pages/2.jpg" class="banner-image" alt="Banner Image">
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-lg-12 mb-2">
                             <div class="card">
                                 <div class="card-body d-flex align-items-center">
@@ -77,7 +77,7 @@ include '../config/databases.php';
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-lg-12 mb-3">
                             <!-- ALL MENU ACCOUNT START -->
                             <?php include "../partials/utils/account-profile.php" ?>
