a8c8355d77c641aa4a40118868236dcfc1fad455
fifovalle <fifanaufal10@gmail.com>
2024-02-29 09:47:05 +0700

    Memodifikasi file analitic

--- src/admin/partials/utils/all-analitic-IKM.php
+++ src/admin/partials/utils/all-analitic-IKM.php
@@ -1,22 +1,17 @@
 <div class="allDataIKM">
-    <div class="card">
-        <h5 class="card-title pt-4 mx-auto">Laporan Data IKM Selama 7 Hari Terakhir</h5>
-        <hr>
-        <div class="d-flex justify-content-around">
-            <div class="row boxData fw-bold">
-                <span class="headerBoxData">Produk</span>
-                <span class="captionBoxData">10<i class="bx bxs-up-arrow-circle captionIcon"></i></span>
-                <span class="performanceBoxData">Lebih Baik</span>
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row"></div>
+                </div>
             </div>
-            <div class="row boxData fw-bold">
-                <span class="headerBoxData">IKM</span>
-                <span class="captionBoxData">10<i class="bx bxs-up-arrow-circle captionIcon"></i></span>
-                <span class="performanceBoxData">Lebih Baik</span>
-            </div>
-            <div class="row boxData fw-bold">
-                <span class="headerBoxData">Transaksi</span>
-                <span class="captionBoxData">10<i class="bx bxs-down-arrow-circle captionIconError"></i></span>
-                <span class="performanceBoxData">Lebih Buruk</span>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row"></div>
+                </div>
             </div>
         </div>
     </div>
--- src/admin/partials/utils/all-analitic-product.php
+++ src/admin/partials/utils/all-analitic-product.php
@@ -1,18 +1,16 @@
 <div class="allDataProduct">
-    <div class="dataAccount">
-        <div class="row">
-            <div class="col-6">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row"></div>
-                    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row"></div>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row"></div>
-                    </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row"></div>
                 </div>
             </div>
         </div>
