6ddf1c30d84537ced3ad77ed857558718bcbbd87
fifovalle <fifanaufal10@gmail.com>
2024-03-02 09:49:44 +0700

    Memodifikasi file profile

--- src/admin/pages/profile.php
+++ src/admin/pages/profile.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['ID'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin PTSP</title>
+    <title>Profil PTSP</title>
     <!-- BOOTSTRAP CSS -->
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <!-- OUR CSS -->
