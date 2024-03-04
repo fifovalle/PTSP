04425d5b198d31b234512542a7cefb5254cfb2f6
fifovalle <fifanaufal10@gmail.com>
2024-03-04 15:22:26 +0700

    Memodifikasi file footer

--- src/admin/partials/components/footer.php
+++ src/admin/partials/components/footer.php
@@ -1,6 +1,6 @@
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-4 my-4 mt-5 border-top">
     <div class="col-4 d-flex align-items-center">
-        <span class="mb-3 mb-md-0 textFooter">Dibuat Oleh Sangar ©
+        <span class="mb-3 mb-md-0 textFooter">Dibuat Oleh Syntax Squad ©
             <script>
                 document.write(new Date().getFullYear())
             </script> PTSP, BMKG
