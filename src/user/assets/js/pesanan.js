160ea0ec61b0d18787963aec4a607315c3ad96e9
fifovalle <fifanaufal10@gmail.com>
2024-03-24 13:06:30 +0700

    Memodifikasi file pesanan

--- src/user/assets/js/pesanan.js
+++ src/user/assets/js/pesanan.js
@@ -1,39 +1,41 @@
 // Get all dot elements
-const dots = document.querySelectorAll('.dot');
+const dots = document.querySelectorAll(".dot");
 
 // Loop through each dot element
-dots.forEach(dot => {
-    // Add click event listener
-    dot.addEventListener('click', () => {
-        // Remove active class from all dots
-        dots.forEach(d => d.classList.remove('active'));
-        // Add active class to the clicked dot
-        dot.classList.add('active');
-    });
+dots.forEach((dot) => {
+  // Add click event listener
+  dot.addEventListener("click", () => {
+    // Remove active class from all dots
+    dots.forEach((d) => d.classList.remove("active"));
+    // Add active class to the clicked dot
+    dot.classList.add("active");
+  });
 });
 function showContentPemesanan(id) {
-    // Semua konten disembunyikan
-    let contentSections = document.querySelectorAll('.col-md-10');
-    contentSections.forEach(function(section) {
-        section.style.display = 'none';
-    });
-    
-    // Tampilkan konten berdasarkan ID yang diberikan
-    document.getElementById(id).style.display = 'block';
+  // Semua konten disembunyikan
+  let contentSections = document.querySelectorAll(".col-md-10");
+  contentSections.forEach(function (section) {
+    section.style.display = "none";
+  });
 
-    // Ubah gaya tombol sesuai dengan ID yang diberikan
-    let buttons = document.querySelectorAll('#opsi-pemesanan .btn');
-    buttons.forEach(function(button) {
-        button.classList.remove('btn-success'); // Hapus kelas btn-success dari semua tombol
-        button.classList.add('btn-outline-success'); // Tambahkan kelas btn-outline-success ke semua tombol
-        button.style.color = 'green';
-    });
+  // Tampilkan konten berdasarkan ID yang diberikan
+  document.getElementById(id).style.display = "block";
 
-    // Tentukan tombol yang diklik
-    let clickedButton = document.getElementById(id === 'history-pesanan' ? 'history-order' : 'tracking-order');
-    clickedButton.classList.add('btn-success');
-    clickedButton.classList.remove('btn-outline-success');
-    clickedButton.style.color = 'white';
+  // Ubah gaya tombol sesuai dengan ID yang diberikan
+  let buttons = document.querySelectorAll("#opsi-pemesanan .btn");
+  buttons.forEach(function (button) {
+    button.classList.remove("btn-success"); // Hapus kelas btn-success dari semua tombol
+    button.classList.add("btn-outline-success"); // Tambahkan kelas btn-outline-success ke semua tombol
+    button.style.color = "green";
+  });
+
+  // Tentukan tombol yang diklik
+  let clickedButton = document.getElementById(
+    id === "history-pesanan" ? "history-order" : "tracking-order"
+  );
+  clickedButton.classList.add("btn-success");
+  clickedButton.classList.remove("btn-outline-success");
+  clickedButton.style.color = "white";
 }
 
 let tombolStatusPengajuan = document.getElementById("btn-status-pengajuan");
@@ -50,56 +52,57 @@ elemenPembayaran.classList.add("d-none");
 elemenPembuatan.classList.add("d-none");
 elemenSelesai.classList.add("d-none");
 
-tombolStatusPengajuan.classList.add('btn-primary');
-tombolStatusPengajuan.style.color = 'white';
+tombolStatusPengajuan.classList.add("btn-primary");
+tombolStatusPengajuan.style.color = "white";
 
-tombolStatusPengajuan.addEventListener("click", function() {
-    elemenAjuan.classList.remove("d-none");
-    elemenPembayaran.classList.add("d-none");
-    elemenPembuatan.classList.add("d-none");
-    elemenSelesai.classList.add("d-none");
+tombolStatusPengajuan.addEventListener("click", function () {
+  elemenAjuan.classList.remove("d-none");
+  elemenPembayaran.classList.add("d-none");
+  elemenPembuatan.classList.add("d-none");
+  elemenSelesai.classList.add("d-none");
 });
 
-tombolStatusPembayaran.addEventListener("click", function() {
-    elemenAjuan.classList.add("d-none");
-    elemenPembayaran.classList.remove("d-none");
-    elemenPembuatan.classList.add("d-none");
-    elemenSelesai.classList.add("d-none");
+tombolStatusPembayaran.addEventListener("click", function () {
+  elemenAjuan.classList.add("d-none");
+  elemenPembayaran.classList.remove("d-none");
+  elemenPembuatan.classList.add("d-none");
+  elemenSelesai.classList.add("d-none");
 });
 
-tombolStatusPembuatan.addEventListener("click", function() {
-    elemenAjuan.classList.add("d-none");
-    elemenPembayaran.classList.add("d-none");
-    elemenPembuatan.classList.remove("d-none");
-    elemenSelesai.classList.add("d-none");
+tombolStatusPembuatan.addEventListener("click", function () {
+  elemenAjuan.classList.add("d-none");
+  elemenPembayaran.classList.add("d-none");
+  elemenPembuatan.classList.remove("d-none");
+  elemenSelesai.classList.add("d-none");
 });
 
-tombolStatusSelesai.addEventListener("click", function() {
-    elemenAjuan.classList.add("d-none");
-    elemenPembayaran.classList.add("d-none");
-    elemenPembuatan.classList.add("d-none");
-    elemenSelesai.classList.remove("d-none");
+tombolStatusSelesai.addEventListener("click", function () {
+  elemenAjuan.classList.add("d-none");
+  elemenPembayaran.classList.add("d-none");
+  elemenPembuatan.classList.add("d-none");
+  elemenSelesai.classList.remove("d-none");
 });
 
-const tombolStatus = document.querySelectorAll('.opsi-statuspesanan');
-tombolStatus.forEach(function(tombol) {
-    tombol.addEventListener('click', function() {
-        tombolStatus.forEach(function(btn) {
-            btn.classList.remove('btn-primary');
-            btn.style.color = ''; 
-        });
-        this.classList.add('btn-primary');
-        this.style.color = 'white'; 
+const tombolStatus = document.querySelectorAll(".opsi-statuspesanan");
+tombolStatus.forEach(function (tombol) {
+  tombol.addEventListener("click", function () {
+    tombolStatus.forEach(function (btn) {
+      btn.classList.remove("btn-primary");
+      btn.style.color = "";
     });
+    this.classList.add("btn-primary");
+    this.style.color = "white";
+  });
 });
 
-document.getElementById("btn-beli-lagi").addEventListener("click", function() {
+document.getElementById("btn-beli-lagi").addEventListener("click", function () {
+  window.location.href = "katalogproduk.php";
+});
+document
+  .getElementById("btn-beli-lagi1")
+  .addEventListener("click", function () {
     window.location.href = "katalogproduk.php";
+  });
+document.getElementById("nilai-ikm").addEventListener("click", function () {
+  window.location.href = "ikm.php";
 });
-document.getElementById("btn-beli-lagi1").addEventListener("click", function() {
-    window.location.href = "katalogproduk.php";
-});
-document.getElementById("nilai-ikm").addEventListener("click", function() {
-    window.location.href = "ikm.php";
-});
-
