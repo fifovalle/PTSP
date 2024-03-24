d8c3d420b4802791bf6f01ac0cc995b483b3f585
fifovalle <fifanaufal10@gmail.com>
2024-03-24 13:02:27 +0700

    Memodifikasi file profile

--- src/user/assets/js/profile.js
+++ src/user/assets/js/profile.js
@@ -1,34 +1,43 @@
 function showProfileSetting(id) {
-    // Semua konten disembunyikan
-    let contentSections = document.querySelectorAll('.col-md-10');
-    contentSections.forEach(function(section) {
-        section.style.display = 'none';
-    });
-    
-    // Tampilkan konten berdasarkan ID yang diberikan
-    document.getElementById(id).style.display = 'block';
+  // Semua konten disembunyikan
+  let contentSections = document.querySelectorAll(".col-md-10");
+  contentSections.forEach(function (section) {
+    section.style.display = "none";
+  });
 
-    // Ubah gaya tombol sesuai dengan ID yang diberikan
-    let buttons = document.querySelectorAll('#opsi-profile .btn');
-    buttons.forEach(function(button) {
-        button.classList.remove('btn-success'); 
-        button.classList.add('btn-outline-success'); 
-        button.style.color = 'green';
-    });
+  // Tampilkan konten berdasarkan ID yang diberikan
+  document.getElementById(id).style.display = "block";
 
-// Determine the clicked button
-let clickedButton;
-if (id === 'opsi-profilesetting') {
-    clickedButton = document.getElementById('profile-setting');
-} else if (id === 'opsi-profileinfo') {
-    clickedButton = document.getElementById('profile-info');
-} else if (id === 'opsi-alamatsetting') {
-    clickedButton = document.getElementById('alamat-setting');
+  // Ubah gaya tombol sesuai dengan ID yang diberikan
+  let buttons = document.querySelectorAll("#opsi-profile .btn");
+  buttons.forEach(function (button) {
+    button.classList.remove("btn-success");
+    button.classList.add("btn-outline-success");
+    button.style.color = "green";
+  });
+
+  // Determine the clicked button
+  let clickedButton;
+  if (id === "opsi-profilesetting") {
+    clickedButton = document.getElementById("profile-setting");
+  } else if (id === "opsi-profileinfo") {
+    clickedButton = document.getElementById("profile-info");
+  } else if (id === "opsi-alamatsetting") {
+    clickedButton = document.getElementById("alamat-setting");
+  }
+
+  // Modify the button's classes and style
+  clickedButton.classList.add("btn-success");
+  clickedButton.classList.remove("btn-outline-success");
+  clickedButton.style.color = "white";
 }
 
-// Modify the button's classes and style
-clickedButton.classList.add('btn-success');
-clickedButton.classList.remove('btn-outline-success');
-clickedButton.style.color = 'white';
+const card = document.querySelector(".card");
+const box = card.querySelector(".box");
+card.addEventListener("mouseenter", function () {
+  box.style.display = "block";
+});
 
-}
+card.addEventListener("mouseleave", function () {
+  box.style.display = "none";
+});
--- src/user/pages/profile.php
+++ src/user/pages/profile.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 ?>
 
 <!DOCTYPE html>
@@ -228,15 +227,16 @@ session_start();
                                                 <option value="2">Perempuan</option>
                                             </select>
                                         </div>
-                                        <div class="col-md-12">
-                                            <label for="suratpengantarInput">Ganti Foto Profile</label>
-                                            <input type="file" class="form-control my-3" id="Ganti_profil" name="Ganti_profil" style="height: 40px">
-                                        </div>
                                     </div>
                                     <div class="col-md-4">
                                         <div class="card">
-                                            <div class="text-center">
+                                            <div class="text-center position-relative">
                                                 <img src="../assets/img/landpage2.jpg" class="img-thumbnail" alt="..." style="border-radius: 50%; width :250px; height:250px; border: none;">
+                                            </div>
+                                            <div class="box position-absolute" style="display: none;">
+                                                <button class="btn border-0 opacity-100" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#gantiProfil">
+                                                    <box-icon name='edit-alt' id="icon-edit" color='rgba(255,255,255,0.9)'></box-icon>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
@@ -285,6 +285,34 @@ session_start();
     </div>
     <script src="../assets/js/navbar.js"></script>
     <script src="../assets/js/profile.js"></script>
+    <div class="modal fade" id="gantiProfil" tabindex="-1" aria-labelledby="gantiProfilLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content" id="ganti_profil">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="gantiProfilLabel">Ganti Profil</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label class="custum-file-upload" for="file">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24">
+                                <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                                <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="text">
+                            <span>Click to upload image</span>
+                        </div>
+                        <input type="file" id="file">
+                    </label>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
