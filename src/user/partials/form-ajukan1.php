50104be2b1cdb532fa8ec63026ec5a49a9db5499
fifovalle <fifanaufal10@gmail.com>
2024-03-24 13:15:27 +0700

    Memodifikasi file form ajukan

--- src/user/partials/form-ajukan1.php
+++ src/user/partials/form-ajukan1.php
@@ -13,7 +13,10 @@
             </div>
             <div class="col-md-6">
                 <label for="phonenumberInput">Nomor Handphone</label>
-                <input type="tel" class="form-control my-3" id="No_HP" name="No_HP" placeholder="Masukkan Nomor Handphone" style="height: 40px">
+                <div class="input-group my-3" style="height: 40px">
+                    <span class="input-group-text spanNumberData">+62</span>
+                    <input type="number" placeholder="Masukkan Nomor Handphone" class="form-control inputData" id="No_HP" name="No_HP" autocomplete="off">
+                </div>
             </div>
             <div class="col-md-6">
                 <label for="emailInput">Email</label>
@@ -36,7 +39,7 @@
     </div>
     <div class="col-md-12 text-center">
         <button class="apply" type="submit" name="Apply">
-            Apply
+            Ajukan Sekarang
             <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
                 <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
             </svg>
