28e47a67011773a85a1f2a1a1ddf5e5755a00b17
fifovalle <fifanaufal10@gmail.com>
2024-03-26 09:48:48 +0700

    Memodifikasi table user di admin

--- src/admin/partials/utils/user-table.php
+++ src/admin/partials/utils/user-table.php
@@ -31,69 +31,70 @@
         $penggunaModel = new Pengguna($koneksi);
         $dataPengguna = $penggunaModel->tampilkanDataPengguna();
         $dataPerusahaan = $penggunaModel->tampilkanDataPerusahaan();
-        if ($dataPengguna !== null && $dataPerusahaan !== null) {
-            $dataGabungan = array_merge($dataPengguna, $dataPerusahaan);
-            function compareByName($a, $b)
-            {
-                return strcmp($a['Nama_Pengguna'] ?? $a['Nama_Pengguna_Anggota_Perusahaan'], $b['Nama_Pengguna'] ?? $b['Nama_Pengguna_Anggota_Perusahaan']);
+        function compareByName($a, $b)
+        {
+            return strcmp($a['Nama_Pengguna'] ?? $a['Nama_Pengguna_Anggota_Perusahaan'], $b['Nama_Pengguna'] ?? $b['Nama_Pengguna_Anggota_Perusahaan']);
+        }
+        if ($dataPengguna !== null || $dataPerusahaan !== null) {
+            if ($dataPengguna !== null && $dataPerusahaan !== null) {
+                $dataGabungan = array_merge($dataPengguna, $dataPerusahaan);
+            } elseif ($dataPengguna !== null) {
+                $dataGabungan = $dataPengguna;
+            } else {
+                $dataGabungan = $dataPerusahaan;
             }
             usort($dataGabungan, 'compareByName');
             $nomorUrut = 1;
-            if (!empty($dataGabungan)) {
-                foreach ($dataGabungan as $data) {
+            foreach ($dataGabungan as $data) {
         ?>
-                    <tr class="trDataN">
-                        <td class="text-center">
-                            <input class="checkBoxData checkBoxDataUser checkBoxDataUserData" type="checkbox">
-                        </td>
-                        <td class="text-center"><?php echo $nomorUrut++; ?></td>
-                        <td class="text-center flex-wrap d-flex justify-content-evenly gap-2">
-                            <div>
-                                <img class="imageData" src="../assets/image/uploads/<?php echo $data['Foto'] ?? $data['Foto_Perusahaan']; ?>" alt="Foto">
+                <tr class="trDataN">
+                    <td class="text-center">
+                        <input class="checkBoxData checkBoxDataUser checkBoxDataUserData" type="checkbox">
+                    </td>
+                    <td class="text-center"><?php echo $nomorUrut++; ?></td>
+                    <td class="text-center flex-wrap d-flex justify-content-evenly gap-2">
+                        <div>
+                            <img class="imageData" src="../assets/image/uploads/<?php echo $data['Foto'] ?? $data['Foto_Perusahaan']; ?>" alt="Foto">
+                        </div>
+                        <div class="deskriptorContainer">
+                            <p class="fw-semibold m-auto"><?php echo $data['Nama_Pengguna'] ?? $data['Nama_Pengguna_Anggota_Perusahaan']; ?></p>
+                            <?php if (isset($data['Nama_Depan_Pengguna'])) : ?>
+                                <p class="fw-semibold deskriptorSmall m-auto"><?php echo $data['Nama_Depan_Pengguna'] . ' ' . $data['Nama_Belakang_Pengguna']; ?></p>
+                            <?php else : ?>
+                                <p class="fw-semibold deskriptorSmall m-auto"><?php echo $data['Nama_Depan_Anggota_Perusahaan'] . ' ' . $data['Nama_Belakang_Anggota_Perusahaan']; ?></p>
+                            <?php endif; ?>
+                            <div class="iconContainerData">
+                                <a class="linkData buttonUser" data-id='<?php echo $data['ID_Pengguna'] ?? $data['ID_Perusahaan']; ?>'>
+                                    <span><i class="fas fa-edit"></i></span>
+                                </a>
+                                <a class="linkData iconDataRight" href="javascript:void(0);" onclick="confirmDeleteUser(<?php echo $data['ID_Pengguna'] ?? $data['ID_Perusahaan']; ?>)">
+                                    <span><i class="fas fa-trash"></i></span>
+                                </a>
                             </div>
-                            <div class="deskriptorContainer">
-                                <p class="fw-semibold m-auto"><?php echo $data['Nama_Pengguna'] ?? $data['Nama_Pengguna_Anggota_Perusahaan']; ?></p>
-                                <?php if (isset($data['Nama_Depan_Pengguna'])) : ?>
-                                    <p class="fw-semibold deskriptorSmall m-auto"><?php echo $data['Nama_Depan_Pengguna'] . ' ' . $data['Nama_Belakang_Pengguna']; ?></p>
-                                <?php else : ?>
-                                    <p class="fw-semibold deskriptorSmall m-auto"><?php echo $data['Nama_Depan_Anggota_Perusahaan'] . ' ' . $data['Nama_Belakang_Anggota_Perusahaan']; ?></p>
-                                <?php endif; ?>
-                                <div class="iconContainerData">
-                                    <a class="linkData buttonUser" data-id='<?php echo $data['ID_Pengguna'] ?? $data['ID_Perusahaan']; ?>'>
-                                        <span><i class="fas fa-edit"></i></span>
-                                    </a>
-                                    <a class="linkData iconDataRight" href="javascript:void(0);" onclick="confirmDeleteUser(<?php echo $data['ID_Pengguna'] ?? $data['ID_Perusahaan']; ?>)">
-                                        <span><i class="fas fa-trash"></i></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </td>
-                        <td class="text-center"><?php echo htmlspecialchars($data['Email_Pengguna'] ?? $data['Email_Perusahaan']); ?></td>
-                        <td class="text-center"><?php echo htmlspecialchars($data['Jenis_Kelamin_Pengguna'] ?? $data['Jenis_Kelamin_Anggota_Perusahaan']); ?></td>
-                        <td class="text-center"><?php echo htmlspecialchars($data['No_Telepon_Pengguna'] ?? $data['No_Telepon_Anggota_Perusahaan']); ?></td>
-                        <td class="text-center">
-                            <?php
-                            $status_pengguna = $data['Status_Verifikasi_Pengguna'] ?? null;
-                            $status_perusahaan = $data['Status_Verifikasi_Perusahaan'] ?? null;
-                            if ($status_pengguna === 'Terverifikasi') {
-                                echo '<span class="badge text-bg-success">Terverifikasi</span>';
-                            } elseif ($status_pengguna !== null || $status_perusahaan !== null) {
-                                echo '<span class="badge text-bg-danger">Belum Terverifikasi</span>';
-                            } else {
-                                echo '<span class="badge text-bg-secondary">Status Tidak Tersedia</span>';
-                            }
-                            ?>
-                        </td>
-                    </tr>
+                        </div>
+                    </td>
+                    <td class="text-center"><?php echo htmlspecialchars($data['Email_Pengguna'] ?? $data['Email_Perusahaan']); ?></td>
+                    <td class="text-center"><?php echo htmlspecialchars($data['Jenis_Kelamin_Pengguna'] ?? $data['Jenis_Kelamin_Anggota_Perusahaan']); ?></td>
+                    <td class="text-center"><?php echo htmlspecialchars($data['No_Telepon_Pengguna'] ?? $data['No_Telepon_Anggota_Perusahaan']); ?></td>
+                    <td class="text-center">
+                        <?php
+                        $status_pengguna = $data['Status_Verifikasi_Pengguna'] ?? null;
+                        $status_perusahaan = $data['Status_Verifikasi_Perusahaan'] ?? null;
+                        if ($status_pengguna === 'Terverifikasi') {
+                            echo '<span class="badge text-bg-success">Terverifikasi</span>';
+                        } elseif ($status_pengguna !== null || $status_perusahaan !== null) {
+                            echo '<span class="badge text-bg-danger">Belum Terverifikasi</span>';
+                        } else {
+                            echo '<span class="badge text-bg-secondary">Status Tidak Tersedia</span>';
+                        }
+                        ?>
+                    </td>
+                </tr>
         <?php
-                }
-            } else {
-                echo "<tr><td colspan='7' class='text-center text-danger fw-bold pt-4 pb-2'>Tidak Ada Data Pengguna atau Perusahaan!</td></tr>";
             }
         } else {
             echo "<tr><td colspan='7' class='text-center text-danger fw-bold pt-4 pb-2'>Tidak ada data pengguna atau data perusahaan!</td></tr>";
         }
         ?>
     </tbody>
-
 </table>
