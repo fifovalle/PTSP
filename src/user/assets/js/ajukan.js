bb8ac9606b11e29ac09efd63bb0a1f43bf4f624b
fifovalle <fifanaufal10@gmail.com>
2024-03-30 12:11:14 +0700

    Menambahkan paduan di setiap pages

--- src/user/assets/js/ajukan.js
+++ src/user/assets/js/ajukan.js
@@ -15,3 +15,101 @@ function showContent(id) {
     var clickedOption = document.querySelector('#button-option button[onclick="showContent(\'' + id + '\')"] .box-option');
     clickedOption.classList.add('selected');
 }
+
+document.querySelector('.btn-ajukantutorial').addEventListener('click', ajukanTutorial);
+function ajukanTutorial() {
+    const driver = window.driver.js.driver;
+    const driverObj = driver({
+        showProgress: true,
+        steps: [{
+                element: '#option',
+                popover: {
+                    title: 'Ajukan Menu',
+                    description: 'Disini adalah menu ajukan yang terdapat pada PTSP BMKG Provinsi Bengkulu sebelum melakukan checkout',
+                    side: "top",
+                    align: 'start'
+                }
+            },
+            {
+                element: '#ajukan1',
+                popover: {
+                    title: 'Kegiatan Penanggulangan Bencana',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan penanggulangan bencana kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "bottom",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#ajukan2',
+                popover: {
+                    title: 'Kegiatan Sosial',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan sosial atau kegiatan kemanusiaan kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "bottom",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#ajukan3',
+                popover: {
+                    title: 'Kegiatan Keagamaan',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan keagamaan kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "bottom",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#ajukan4',
+                popover: {
+                    title: 'Kegiatan Pertahanan dan Keamanan',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan pertahanan dan keamanan yang melibatkan institusi keamanan kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "right",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#ajukan5',
+                popover: {
+                    title: 'Kegiatan Pendidikan dan Penilitian non Komersil',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan penanggulangan bencana kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "bottom",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#ajukan6',
+                popover: {
+                    title: 'Pemerintah Pusat atau Daerah',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan penanggulangan bencana kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "bottom",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#ajukan7',
+                popover: {
+                    title: 'Pelayanan Informasi dengan Tarif PNBP',
+                    description: 'Ini adalah form untuk mengajukan kebutuhan informasi/jasa kegiatan penanggulangan bencana kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "bottom",
+                    align: 'center'
+                }
+            },
+            {
+                element: '#form-ajukan',
+                popover: {
+                    title: 'Form Ajukan',
+                    description: 'Ini adalah form ajukan kepada PTSP BMKG Provinsi Bengkulu',
+                    side: "top",
+                    align: 'start'
+                }
+            },
+            {
+                popover: {
+                    title: 'Welcome To PTSP BMKG Provinsi Bengkulu',
+                    description: 'Selamat menikmati dan selamat menggunakan fitur dan fasilitas dari kami. Terima kasih',
+                    align: 'center'
+                }
+            }
+        ]
+    });
+    driverObj.drive();
+}
--- src/user/pages/ajukan.php
+++ src/user/pages/ajukan.php
@@ -21,69 +21,87 @@ session_start();
     ?>
     <div class="container-fluid mt-5">
         <div class="row" id="option">
-            <div class="col" id="button-option">
-                <button onclick="showContent('form1')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success z-3">
-                        GRATIS
-                    </span>
-                    <span class="box-option selected">
-                        Kegiatan Penanggulangan Bencana
-                    </span>
-                </button>
-                <button onclick="showContent('form2')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success z-3">
-                        GRATIS
-                    </span>
-                    <span class="box-option">
-                        Kegiatan Sosial
-                    </span>
-                </button>
-                <button onclick="showContent('form3')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success z-3">
-                        GRATIS
-                    </span>
-                    <span class="box-option">
-                        Kegiatan Keagamaan
-                    </span>
-                </button>
-                <button onclick="showContent('form4')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success z-3">
-                        GRATIS
-                    </span>
-                    <span class="box-option">
-                        Kegiatan Pertahanan dan Keamanan
-                    </span>
-                </button>
+            <div class="col-md-12" id="button-option">
+                <div class="row">
+                    <div class="col-md-3 pt-2 " id="ajukan1">
+                        <button onclick="showContent('form1')">
+                            <span class="translate-middle badge rounded-pill bg-success">
+                                GRATIS
+                            </span>
+                            <span class="box-option selected">
+                                Kegiatan Penanggulangan Bencana
+                            </span>
+                        </button>
+                    </div>
+                    <div class="col-md-3 pt-2" id="ajukan2">
+                        <button onclick="showContent('form2')">
+                            <span class="translate-middle badge rounded-pill bg-success">
+                                GRATIS
+                            </span>
+                            <span class="box-option">
+                                Kegiatan Sosial
+                            </span>
+                        </button>
+                    </div>
+                    <div class="col-md-3 pt-2" id="ajukan3">
+                        <button onclick="showContent('form3')">
+                            <span class="translate-middle badge rounded-pill bg-success">
+                                GRATIS
+                            </span>
+                            <span class="box-option">
+                                Kegiatan Keagamaan
+                            </span>
+                        </button>
+                    </div>
+                    <div class="col-md-3 pt-2" id="ajukan4">
+                        <button onclick="showContent('form4')">
+                            <span class="translate-middle badge rounded-pill bg-success">
+                                GRATIS
+                            </span>
+                            <span class="box-option">
+                                Kegiatan Pertahanan dan Keamanan
+                            </span>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div class="col" id="button-option">
-                <button onclick="showContent('form5')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success z-3">
-                        GRATIS
-                    </span>
-                    <span class="box-option">
-                        Kegiatan Pendidikan dan Penilitian non Komersil
-                    </span>
-                </button>
-                <button onclick="showContent('form6')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success z-3">
-                        GRATIS
-                    </span>
-                    <span class="box-option">
-                        Pemerintah Pusat atau Daerah
-                    </span>
-                </button>
-                <button onclick="showContent('form7')">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger z-3">
-                        BERBAYAR
-                    </span>
-                    <span class="box-option">
-                        Pelayanan Informasi dengan Tarif PNBP
-                    </span>
-                </button>
+            <div class="col-md-12 mt-5 mx-5" id="button-option">
+                <div class="row">
+                    <div class="col-md-4 pt-2" id="ajukan5">
+                        <button onclick="showContent('form5')">
+                            <span class="translate-middle badge rounded-pill bg-success">
+                                GRATIS
+                            </span>
+                            <span class="box-option">
+                                Kegiatan Pendidikan dan Penilitian non Komersil
+                            </span>
+                        </button>
+                    </div>
+                    <div class="col-md-4 pt-2" id="ajukan6">
+                        <button onclick="showContent('form6')">
+                            <span class="translate-middle badge rounded-pill bg-success">
+                                GRATIS
+                            </span>
+                            <span class="box-option">
+                                Pemerintah Pusat atau Daerah
+                            </span>
+                        </button>
+                    </div>
+                    <div class="col-md-4 pt-2" id="ajukan7">
+                        <button onclick="showContent('form7')">
+                            <span class=" top-0 start-0 translate-middle badge rounded-pill bg-danger">
+                                BERBAYAR
+                            </span>
+                            <span class="box-option">
+                                Pelayanan Informasi dengan Tarif PNBP
+                            </span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="row">
+    <div class="row" id="form-ajukan">
         <div class="form-content" id="form1">
             <?php
             include('../partials/form-ajukan1.php');
@@ -119,6 +137,11 @@ session_start();
             include('../partials/form-ajukan7.php');
             ?>
         </div>
+    </div>
+    <div class="button-container">
+        <button class="z-3 btn-ajukantutorial">
+            <box-icon name='question-mark' class="svgIcon" color='rgba(255,255,255,0.9)'></box-icon>
+        </button>
     </div>
     <script src="../assets/js/navbar.js"></script>
     <script src="../assets/js/ajukan.js"></script>
