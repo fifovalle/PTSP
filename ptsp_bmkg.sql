1d8d0574d4b98ab1f47aa3db04df6937cd6d638e
fifovalle <fifanaufal10@gmail.com>
2024-03-31 15:32:52 +0700

    Memodifikasi file sql

--- ptsp_bmkg.sql
+++ ptsp_bmkg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2024 at 10:24 AM
+-- Generation Time: Mar 31, 2024 at 10:32 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
