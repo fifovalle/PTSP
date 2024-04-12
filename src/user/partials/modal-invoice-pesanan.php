e1cfeb4404b34fc8a14e15d62fa42743e73e49d6
fifovalle <fifanaufal10@gmail.com>
2024-04-12 20:36:51 +0700

    Menambahkan file modal invoice

---
+++ src/user/partials/modal-invoice-pesanan.php
@@ -0,0 +1,76 @@
+<div class="modal fade" id="invoicePesanan" tabindex="-1" aria-labelledby="invoicePesananLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content" id="box-perbaikan">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="invoicePesananLabel">Pesanan #178VBYDG523</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h4 class="mb-4">INFORMASI PESANAN</h4>
+                <h6 class="fw-bold mt-4">PENERIMA INSTANSI A</h6>
+                <table class="table" style="border-radius: 10px;">
+                    <thead class="table-secondary">
+                        <tr>
+                            <td>Produk</td>
+                            <td>Rekening</td>
+                            <td>Harga</td>
+                            <td>Jumlah</td>
+                            <td>Total</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="produk ps-3">Data 1</td>
+                            <td class="rekening ps-3">1111</td>
+                            <td class="harga ps-3">Rp 100.000</td>
+                            <td class="jumlah ps-3">5</td>
+                            <td class="total ps-3">Rp 100.000</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h6 class="fw-bold mt-4">PENERIMA INSTANSI B</h6>
+                <table class="table">
+                    <thead class="table-secondary">
+                        <tr>
+                            <td>Produk</td>
+                            <td>Rekening</td>
+                            <td>Harga</td>
+                            <td>Jumlah</td>
+                            <td>Total</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="produk ps-3">Data 1</td>
+                            <td class="rekening ps-3">1111</td>
+                            <td class="harga ps-3">Rp 100.000</td>
+                            <td class="jumlah ps-3">5</td>
+                            <td class="total ps-3">Rp 100.000</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h6 class="fw-bold mt-4">PENERIMA INSTANSI C</h6>
+                <table class="table">
+                    <thead class="table-secondary">
+                        <tr>
+                            <td>Produk</td>
+                            <td>Rekening</td>
+                            <td>Harga</td>
+                            <td>Jumlah</td>
+                            <td>Total</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="produk ps-3">Data 1</td>
+                            <td class="rekening ps-3">1111</td>
+                            <td class="harga ps-3">Rp 100.000</td>
+                            <td class="jumlah ps-3">5</td>
+                            <td class="total ps-3">Rp 100.000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
