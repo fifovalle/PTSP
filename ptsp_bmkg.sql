bad52e62e0d6049ab869d4dd62f8805ae66ef57e
Naufal FIFA <90078068+fifovalle@users.noreply.github.com>
2024-04-20 13:51:31 +0700

    Memodifikasi file sql

--- ptsp_bmkg.sql
+++ ptsp_bmkg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2024 at 02:33 PM
+-- Generation Time: Apr 20, 2024 at 08:51 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
