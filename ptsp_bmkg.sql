fb3595ead9a95d4c48444e2ad19b366c625719e3
fifovalle <fifanaufal10@gmail.com>
2024-07-28 21:03:31 +0700

    Memperbaiki sql

--- ptsp_bmkg.sql
+++ ptsp_bmkg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2024 at 03:45 PM
+-- Generation Time: Jul 28, 2024 at 04:03 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
